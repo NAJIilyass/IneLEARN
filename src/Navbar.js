5d89b39e1c853fd8ba3e9ac758660607c699b2e8
IlYess <najiilyassoo@gmail.com>
2024-02-23 03:19:28 +0100

    IneLearn final commit

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <h1>This is the Navbar page</h1>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import HeaderAvant from "./HeaderAvant";
+//import './js'
+import HeaderAfter from "./HeaderAfter";
+
+var connected = localStorage.getItem("connected");
+var head;
+if (!connected) {
+    head = <HeaderAvant />;
+} else {
+    head = <HeaderAfter />;
+}
+
+function HomePage() {
+    return <div>{head}</div>;
+}
+
+export default HomePage;
